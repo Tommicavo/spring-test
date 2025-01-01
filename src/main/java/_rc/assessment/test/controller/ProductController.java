3f586b93ae2a1bb6e5743fa0149e78131d47d5a1
Tommaso Cavini <t.cavini@reply.it>
2025-01-01 19:27:17 +0100

    added validation

--- src/main/java/_rc/assessment/test/controller/ProductController.java
+++ src/main/java/_rc/assessment/test/controller/ProductController.java
@@ -2,8 +2,10 @@ package _rc.assessment.test.controller;
 
 import _rc.assessment.test.model.Product;
 import _rc.assessment.test.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -29,11 +31,17 @@ public class ProductController {
     @GetMapping("/products/add")
     public String addProductForm(Model model) {
         model.addAttribute("product", new Product());
-        return "products/add";
+        model.addAttribute("create", true);
+        return "products/productForm";
     }
 
     @PostMapping("/products")
-    public String saveProduct(@ModelAttribute Product product, Model model) {
+    public String saveProduct(Model model, @Valid @ModelAttribute Product product, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("product", product);
+            model.addAttribute("create", true);
+            return "products/productForm";
+        }
         productService.saveProduct(product);
         return listProducts(model);
     }
@@ -41,12 +49,17 @@ public class ProductController {
     @GetMapping("/products/edit/{id}")
     public String editProductForm(@PathVariable Long id, Model model) {
         productService.getProductById(id).ifPresent(product -> model.addAttribute("product", product));
-        return "products/edit";
+        model.addAttribute("create", false);
+        return "products/productForm";
     }
 
     @PostMapping("/products/{id}")
-    public String updateProduct(@PathVariable Long id, @ModelAttribute Product product, Model model) {
-        product.setId(id);
+    public String updateProduct(Model model, @Valid @ModelAttribute Product product, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("product", product);
+            model.addAttribute("create", false);
+            return "products/productForm";
+        }
         productService.saveProduct(product);
         return listProducts(model);
     }
